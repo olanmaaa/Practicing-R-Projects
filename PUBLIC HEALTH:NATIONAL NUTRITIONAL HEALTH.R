63b7d4edbfa8288aefcb5b9ef816939668b9ce67
Olanma <olanmasolace@gmail.com>
2025-09-21 13:28:00 +0000

    installing package for data visulaisation

--- PUBLIC HEALTH:NATIONAL NUTRITIONAL HEALTH.R
+++ PUBLIC HEALTH:NATIONAL NUTRITIONAL HEALTH.R
@@ -3,3 +3,11 @@
 nhanes.data <- (read.csv("https://raw.githubusercontent.com/HackBio-Internship/public_datasets/main/R/nhanes.csv"))
 #removing all the rows with missing data signified by 'N/A'
 cleaned_nhanes <- na.omit(nhanes.data)
+#installing ggplot for the purpose of data visualization
+install.packages("ggplot2")
+library (ggplot2)
+
+
+
+
+
