bd01f65398a8f2ac3b49a29d2c919471170e4330
Olanma <olanmasolace@gmail.com>
2025-09-21 13:00:37 +0000

    step one: data cleaning

--- PUBLIC HEALTH:NATIONAL NUTRITIONAL HEALTH.R
+++ PUBLIC HEALTH:NATIONAL NUTRITIONAL HEALTH.R
@@ -1,1 +1,5 @@
 #public health: national nutritional health
+#importing data set
+nhanes.data <- (read.csv("https://raw.githubusercontent.com/HackBio-Internship/public_datasets/main/R/nhanes.csv"))
+#removing all the rows with missing data signified by 'N/A'
+cleaned_nhanes <- na.omit(nhanes.data)
