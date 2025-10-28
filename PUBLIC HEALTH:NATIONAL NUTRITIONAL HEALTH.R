f3594f1490d1330a52f00c034246cd2570aa049e
Olanma <olanmasolace@gmail.com>
2025-10-28 20:15:49 +0000

    edit made to initial commits

--- PUBLIC HEALTH:NATIONAL NUTRITIONAL HEALTH.R
+++ PUBLIC HEALTH:NATIONAL NUTRITIONAL HEALTH.R
@@ -1,8 +1,8 @@
 #public health: national nutritional health
 #importing data set
 nhanes.data <- (read.csv("https://raw.githubusercontent.com/HackBio-Internship/public_datasets/main/R/nhanes.csv"))
-#removing all the rows with missing data signified by 'N/A'
-cleaned_nhanes <- na.omit(nhanes.data)
+#replacing all missing data with '0' 
+cleaned_nhanes <- replace(nhanes.data, is.na(nhanes.data), 0)
 
 #installing ggplot for the purpose of data visualization
 install.packages("ggplot2")
